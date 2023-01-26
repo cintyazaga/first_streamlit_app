771902c578f14252743238c33415aef4a8146ced
cintyazaga <123589451+cintyazaga@users.noreply.github.com>
2023-01-26 11:15:27 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
-
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My parents New Healtly DIner')
 
@@ -10,23 +13,23 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruist_list =my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_to_show=my_fruit_list.loc[fruits_selected]
 
 #Display the table on the page
-streamlit.dataframe(my_fruit_list)
-import snowflake.connector
+streamlit.dataframe(fruits_to_show)
 
 #New section to display fruitvice api source
 streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice=streamlit.text_input('What fruit would you like information about?','Jackfruit')
+#me falta un codigo
 
-
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 streamlit.write('The user entered ')
 
