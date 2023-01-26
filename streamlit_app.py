482b652728e1143ad04bf5ec287aefa69314e05b
cintyazaga <123589451+cintyazaga@users.noreply.github.com>
2023-01-26 10:14:51 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,9 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 import snowflake.connector
 
-#New section to display fruitvice api source 
+#New section to display fruitvice api source
+streamlit.header('Fruityvice Fruit Advice!')
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
